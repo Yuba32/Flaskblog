dee58622e3314673a77776906fcc44e4132bd86c
Yuba32 <92969369+Yuba32@users.noreply.github.com>
2022-05-18 13:45:46 +0900

    [update] Step6

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
@@ -25,6 +27,20 @@ def article1():
 @app.route("/article2")
 def article2():
     return render_template("article2.html")
+
+@app.route("/create",methods=["GET","POST"])
+def create():
+    if request.method == "POST":
+        title = request.form.get("title")
+        body = request.form.get("body")
+
+        post = Post(title=title,body=body)
+
+        db.session.add(post)
+        db.session.commit()
+        return redirect("/")
+    else:
+        return render_template("create.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
